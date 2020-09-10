2312f16c4b2304c0ad96cebe0f0bbbba899f527e
sangheonlee <sanghun0724@naver.com>
2020-09-11 00:38:56 +0900

    Initial Commit

---
+++ FirstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftUI
+//
+//  Created by sangheon on 2020/09/11.
+//  Copyright © 2020 sangheon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
